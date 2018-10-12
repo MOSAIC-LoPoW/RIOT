74c9226b7ddd183ad360f4b42cac3abfd49fcb98
matsdemeyer <matsdemeyer@gmail.com>
2018-10-12 10:35:28 +0200

    added octa board.c file

--- applications/octa-test/main.c
+++ applications/octa-test/main.c
@@ -9,17 +9,33 @@
 
 #include <stdio.h>
 #include "board.h"
+#include "xtimer.h"
+
+#define SLEEP       (100 * 1000U)
 
 int main(void)
 {
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-    //turn on R led
-    LED0_ON;
-    //turn on G led
-    LED1_ON;
-    //turn on B led
-    LED2_ON;
+
+    while(1)
+    {
+        //turn on R led
+        LED0_ON;
+        xtimer_usleep(SLEEP);
+        LED0_OFF;
+        //turn on G led
+        LED1_ON;
+        xtimer_usleep(SLEEP);
+        LED1_OFF;
+        //turn on B led
+        LED2_ON;
+        xtimer_usleep(SLEEP);
+        LED2_OFF;
+    }
+    
+
+    
 
 
     return 0;
